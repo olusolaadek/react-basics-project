f57787defee2c23245656711467a4ca4b1019371
FESTUS ADEKUNLE <olusolaadek@hotmail.com>
2023-05-21 22:57:52 +0100

    Complete sample project

--- src/UI/ErrorModal.jsx
+++ src/UI/ErrorModal.jsx
@@ -9,29 +9,17 @@ const ErrorModal = (props) => {
     <div>
       <div className={classes.backdrop} onClick={props.onConfirm}>
         <Card className={classes.modal}>
-          <header className={classes.header}>{props.title}</header>
+          <header className={classes.header}>
+            <h2> {props.title}</h2>{" "}
+          </header>
           <div className={classes.content}>
-            <p>{props.message}</p>
+            <p className="text-danger">{props.message}</p>
           </div>
           <footer className={classes.actions}>
             <Button onClick={props.onConfirm}>Okay</Button>
           </footer>
         </Card>
       </div>
-      {/* {props.showError && (
-        <div className={classes.backdrop}>
-          <div className={classes.modal}>
-            <div className={classes.header}>
-              {" "}
-              <h2>Error </h2>
-            </div>
-            <div className={classes.content}>{props.errorMessage}</div>
-          </div>
-          <div className={classes.actions}>
-            <button onClick={dismissHandler}>X</button>
-          </div>
-        </div>
-      )} */}
     </div>
   );
 };
